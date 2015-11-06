a25d5f77ca031f20398a99284709d097414b0520
Hoang.Phan <phantichhoang@gmail.com>
2015-11-06 14:35:38 +0700

    Add source in zip file for DataBinding Demo Session01

--- Tutor01Day01_DataBindingDemo/databindingdemo/src/main/java/com/n9xkun/iziroi/tutor01_databindingdemo/model/UserBean.java
+++ Tutor01Day01_DataBindingDemo/databindingdemo/src/main/java/com/n9xkun/iziroi/tutor01_databindingdemo/model/UserBean.java
@@ -1,7 +1,22 @@
+/**
+ * http://iziroi.9xkun.com
+ *
+ * LICENSE
+ *
+ * This source file is belong to iziroi.9xkun.com. Please come to this site and
+ * get more source code. If you have any question, please send email to me at:
+ * phantichhoang@gmail.com
+ *
+ * @copyright Copyright (c) 2013-2014 iziroi
+ * @author hoangpt
+ * @version $Id$
+ * @since
+ */
 package com.n9xkun.iziroi.tutor01_databindingdemo.model;
 
 /**
- * Created by hoangpt on 10/9/15.
+ * Domain User (Bean)
+ * @author hoangpt
  */
 public class UserBean {
   private String firstName;
--- Tutor01Day02_DataBindingDemo/databindingdemo/src/main/java/com/n9xkun/iziroi/tutor01_databindingdemo/model/UserBean.java
+++ Tutor01Day02_DataBindingDemo/databindingdemo/src/main/java/com/n9xkun/iziroi/tutor01_databindingdemo/model/UserBean.java
@@ -1,3 +1,17 @@
+/**
+ * http://iziroi.9xkun.com
+ *
+ * LICENSE
+ *
+ * This source file is belong to iziroi.9xkun.com. Please come to this site and
+ * get more source code. If you have any question, please send email to me at:
+ * phantichhoang@gmail.com
+ *
+ * @copyright Copyright (c) 2013-2014 iziroi
+ * @author hoangpt
+ * @version $Id$
+ * @since
+ */
 package com.n9xkun.iziroi.tutor01_databindingdemo.model;
 
 /**
